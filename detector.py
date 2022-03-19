8410fc11a920c592ec95ba0b1d14728e472a8f79
gtoumbas <gtoumbas@princeton.edu>
2022-03-18 21:55:48 -0400

    detector should be working, might need to clean up a bit

--- detector.py
+++ detector.py
@@ -1,4 +1,5 @@
 from sys import argv
+import ipaddress
 import dpkt
 
 
@@ -10,10 +11,51 @@ def detect_anomaly(packet_capture):
     :param packet_capture: dpkt packet capture object for processing
     """
 
-    # implement your logic here and remove the print statement
-    print('not implemented\n')
+    # Keys are IP and vals are [num SYN sent, num SNY+ACK received]
+    ips = {} 
+    for ts, buf in packet_capture:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf) 
+        except dpkt.dpkt.NeedData: # malformed packet 
+            continue
+
+        ip = eth.data
+
+        # TODO probably a better way to do this
+        # Checking for ethernet and IP
+        if type(eth).__name__ != "Ethernet" or type(ip).__name__ != "IP":
+            continue
+
+        # Checking for TCP 
+        if dpkt.ip.get_ip_proto_name(ip.p) != "TCP":
+            continue
+
+        tcp = ip.data
+        flags = dpkt.tcp.tcp_flags_to_str(tcp.flags).split(",")
+
+        sa_counts_src = ips.get(ip.src) if ips.get(ip.src) else [0, 0]
+        sa_counts_dst = ips.get(ip.dst) if ips.get(ip.dst) else [0, 0]
+
+        syn =  "SYN" in flags 
+        ack = "ACK" in flags
+        
+        # Update dict values if needed
+        if syn and ack:
+            sa_counts_dst[1] += 1
+            ips[ip.dst] = sa_counts_dst
+        elif syn:
+            sa_counts_src[0] += 1
+            ips[ip.src] = sa_counts_src
 
 
+    # Checking relative amounts of SYN vs SYN+ACK
+    for key, value in ips.items():
+        num_syn = value[0]
+        num_syn_ack =  value[1]
+
+        if num_syn > 3 * num_syn_ack:
+            print(ipaddress.IPv4Address(key))
+ 
 # parse the command line argument and open the file specified
 if __name__ == '__main__':
     if len(argv) != 2:
